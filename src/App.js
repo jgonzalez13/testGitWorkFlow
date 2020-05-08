7e08d129bff4be2e9164c8a0b2c5c1a42898d13e
Norman <normanLau@users.noreply.gitlabhmhsistemas.ddns.net>
2020-05-07 21:09:58 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Saludo from './componets/Saludo'
+import Nav from './componets/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Saludo name="Emmanuel" />
+      <Nav></Nav>
     </div>
   );
 }
---
+++ src/componets/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return ( 
+        <div className="nav">
+            Aquí el navbar
+        </div>
+     );
+}
+ 
+export default Nav;
