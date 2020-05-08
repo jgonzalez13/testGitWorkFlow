8b14d1d0f29ac1adfde2a3f873931ac4f699e3ef
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-05-07 20:59:38 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Saludo from './componets/Saludo'
+import Footer from './componets/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Saludo name="Emmanuel" />
+      <Saludo name="Emmanuel" /> 
+      <Footer />
     </div>
   );
 }
