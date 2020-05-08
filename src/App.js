12684e7a79230ba77cea74ed7fc80a58ee8b2190
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-05-07 21:19:00 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+
 import React from 'react';
 import Saludo from './componets/Saludo'
+import Head from './header'
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
+      <Head />
       <Saludo name="Emmanuel" />
     </div>
   );
